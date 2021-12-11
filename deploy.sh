516683e2ee915fbf02260509e0db4792af74a3ea
Ethan Brace <ethanabrace@gmail.com>
2021-12-11 01:36:07 -0500

    remove app volume before restarting

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR;
 
 docker-compose pull php
+docker-compose down
+docker volume rm ethanbracecom_app
 docker-compose up -d --force-recreate
