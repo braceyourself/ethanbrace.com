421b6c84d014341b06f909067ac801e8aafcdbde
Ethan Brace <ethanabrace@gmail.com>
2021-12-13 12:56:10 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,5 @@ SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR;
 
-docker-compose pull php
-docker-compose down
-docker volume rm ethanbracecom_app
+docker-compose pull -q php
 docker-compose up -d --force-recreate
